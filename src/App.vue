b308d2b1ef3b1d3f8d42307d77ebc8b7c4d3c885
Portunis <petrovpavlick@mail.ru>
2022-04-28 16:51:55 +0700

    [chart-ui] Добавил placeholder loader, подтянул данные с бд firestore

--- src/App.vue
+++ src/App.vue
@@ -10,35 +10,14 @@ import { defineComponent } from "vue";
 
 import BadgeInfo from "@/components/UI/badge/BadgeInfo.vue";
 import NavBar from "@/components/NavBar/NavBar.vue";
-import TaskTASK from "@/typescript/Task";
-import { mapActions } from "pinia";
-import { useBoardStore } from "@/store/board";
-const testClasses = new TaskTASK(
-  1,
-  "name",
-  "qwe",
-  213,
-  "cea",
-  "23",
-  12312,
-  345,
-  234
-);
+
 export default defineComponent({
   components: { NavBar, BadgeInfo },
-  created() {
-    testClasses.testFn();
-    this.initBoard();
-  },
+
   computed: {
     layout: function () {
       return this.$route.meta.layout || "default-layout";
     },
-  },
-  methods: {
-    ...mapActions(useBoardStore, {
-      initBoard: "getLocalStorage",
-    }),
   },
 });
 </script>
